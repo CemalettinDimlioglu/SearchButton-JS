0df4247609f575f7875666aaeb96863831a8ff9f
CMLTTN <cemaldim@gmail.com>
2023-06-01 14:34:15 +0300

    info

--- app.js
+++ app.js
@@ -7,3 +7,9 @@ btn.addEventListener("click",() => {
      input.focus();
 
 });
+
+ 
+// Bu JavaScript kod bloğu, .btn öğesine tıklandığında .search öğesine .active sınıfını ekleyip çıkarmayı ve .input öğesine odaklanmayı sağlar.
+// .querySelector() yöntemi kullanılarak, .search, .input ve .btn öğeleri DOM'da bulunur.
+// .addEventListener() yöntemi kullanılarak, .btn öğesine bir tıklama olayı eklenir. Tıklama olayı tetiklendiğinde, bir arrow işlevi kullanılır ve bu işlev .classList.toggle() yöntemini çağırarak .search öğesine .active sınıfını ekler veya çıkarır. Böylece arama kutusu genişleyip daralır.
+// Daha sonra, .input öğesine .focus() yöntemi çağrılarak odaklanılır, böylece kullanıcı arama kutusuna hemen metin girebilir.
